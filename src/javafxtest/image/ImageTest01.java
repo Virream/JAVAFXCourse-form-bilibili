4f268d968c417021d64feeee387b0f2a5dda7f06
Virream <1828747201@qq.com>
2023-09-17 15:43:37 +0800

    对Image类的测试,删除了一张图片

---
+++ src/javafxtest/image/ImageTest01.java
@@ -0,0 +1,53 @@
+package javafxtest.image;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
+
+//image类的测试
+public class ImageTest01 extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        AnchorPane anchorPane = new AnchorPane();
+        Scene scene = new Scene(anchorPane);
+
+        //image支持的格式有png,jpg,gif
+        /*image类参数(参数1文件路径:可以接收一个fileInputStream或string
+        *            参数2设置宽:接收一个double
+        *            参数3设置高:接收一个double
+        *            参数4是否开启等比缩放:接收bool(假如宽和高的数值均不是0,那么会优先使用较小的数值进行比例约束)
+        *            参数5是否高质量缩放:接收bool
+        *            参数6是否异步加载图片:接收bool(开启后就会像网页一样,图片可以慢慢加载不影响用户交互)
+        * */
+
+        FileInputStream fileInputStream = new FileInputStream(new File("E:\\照片\\img0.jpg"));
+        Image image1 = new Image("res/img1.jpg",0,500,true,true);
+
+        String url = "file:E:\\照片\\img0.jpg";//资源路径可以是网络地址也可以是本地地址，假如是本地地址前面要加上file:
+        Image image2 = new Image(url,700,300,true,true,true);
+
+        //通过类加载器获取资源路径
+        URL imgUrl = getClass().getClassLoader().getResource("res/img1.jpg");
+        //我对类加载器不太熟悉,我这里加载失败了,这样写估计是从当前类的位置开始寻找文件
+        URL imgUrl2 = getClass().getResource("res/img1.jpg");
+        String urlString = imgUrl.toExternalForm();//转成以file:开头的字符串
+        Image image3 = new Image(urlString,550,0,true,true,true);
+
+        ImageView imageView = new ImageView(image3);
+        anchorPane.getChildren().addAll(imageView);
+        primaryStage.setScene(scene);
+        primaryStage.setWidth(880);
+        primaryStage.setHeight(600);
+        primaryStage.show();
+    }
+}
---
+++ src/javafxtest/image/ImageTest02.java
@@ -0,0 +1,61 @@
+package javafxtest.image;
+
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+//2023年9月17日15:42:37
+//next:p68
+public class ImageTest02 extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        AnchorPane anchorPane = new AnchorPane();
+        Scene scene = new Scene(anchorPane);
+
+        URL url1 = getClass().getClassLoader().getResource("res/img2.jpg");
+        Image image1 = new Image(url1.toExternalForm(),700,700,true,true);
+        Image image2 = new Image("file:res/img2.jpeg",700,700,true,true,true);
+        System.out.println(image1.getHeight());//获取当前显示的高(异步加载可能会失效)
+        System.out.println(image1.getWidth());//获取当前显示的宽(异步加载可能会失效)
+        System.out.println(image1.getRequestedHeight());//获取图片的高
+        System.out.println(image1.getRequestedWidth());//获取图片的宽
+
+        //加载错误监听
+        image2.errorProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                System.out.println("加载是否出错?" + newValue);
+            }
+        });
+        //异常信息打印
+        image2.exceptionProperty().addListener(new ChangeListener<Exception>() {
+            @Override
+            public void changed(ObservableValue<? extends Exception> observable, Exception oldValue, Exception newValue) {
+                System.out.println(newValue);
+            }
+        });
+        //加载进度监听,仅当开启异步加载可用
+        image2.progressProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                System.out.println(newValue);
+            }
+        });
+
+        ImageView imageView = new ImageView(image1);
+        anchorPane.getChildren().addAll(imageView);
+        primaryStage.setScene(scene);
+        primaryStage.setHeight(735);
+        primaryStage.setWidth(1230);
+        primaryStage.show();
+    }
+}
